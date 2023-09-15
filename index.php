33dd446ee6df8aa05994633eef9a8fc72f8fbff6
MIGUEL DE PAULA VELOSO <miguel.pveloso@senacsp.edu.br>
2023-09-14 22:07:42 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>03 pi</title>
+</head>
+<body>
+    <div class="topo">
+    <div class="logo">
+        <img id="logo_site" src="images/corinthians.png">
+    </div>
+    <div class="menu"></div>
+    </div>
+
+    <div class="conteudo"></div>
+
+    <footer>
+        
+    </footer>
+    
+</body>
+</html>
