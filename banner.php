d61e2a88bbf01207c825bea0fed402fecf72700b
MIGUEL DE PAULA VELOSO <miguel.pveloso@senacsp.edu.br>
2023-09-21 21:25:35 -0300

    up

--- banner.php
+++ banner.php
@@ -3,6 +3,7 @@
 $banner_titulo = $_POST['campo-titulo'];
 $banner_descricao = $_POST['campo-descricao'];
 $banner_imagem = $_FILES['campo-imagem'];
+$banner_diretorio = 'images/'
 
 // Credenciais da Base de Dados:
 $servidor = 'localhost';
@@ -11,14 +12,24 @@ $usuario = 'bd_publicidade';
 $senha = 'P@ssw0rd';
 
 // Criar a CONEXÃO com a Base de Dados:
-
+$conexao = new mysqli ($servidor, $usuario, $senha, $base)
 
 // Criar e Excecutar a SQL para INSERIR os dados da tabela:
 $sql = 'INSERT INTO tbl_banners(titulo, descricao, imagem) VALUES ("' . $banner_titulo . '", "' . $banner_descricao . '", "' . $banner_imagem . '");';
 
 
 // Encerrar CONEXÃO com a Base de Dados:
-$conexao.close();
+$conexao->close($sql);
+
+//upload
+if (move_uploaded_file($banner_imagem['tmp_name'], '$banner_diretorio'.$banner_imagem['name']))
+{
+    echo 'imagem enviada com sucesso!';
+}
+else
+{
+    echo 'imagem nao enviada';
+}
 
 // Direcionar o usuário de volta a página inicial:
 header('Location: ');
